33828f9d00198a37fa79d4875bc375f38f597fe4
Onat Ender <152275242+onatender@users.noreply.github.com>
2024-02-02 21:23:47 +0300

    Add files via upload

---
+++ FileExplorer/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace FileExplorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtQuery = new System.Windows.Forms.TextBox();
+            this.lstFile = new System.Windows.Forms.ListBox();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.strpthexp = new System.Windows.Forms.Button();
+            this.wholepccb = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtQuery
+            // 
+            this.txtQuery.Location = new System.Drawing.Point(25, 43);
+            this.txtQuery.Name = "txtQuery";
+            this.txtQuery.Size = new System.Drawing.Size(289, 20);
+            this.txtQuery.TabIndex = 0;
+            // 
+            // lstFile
+            // 
+            this.lstFile.FormattingEnabled = true;
+            this.lstFile.Location = new System.Drawing.Point(25, 88);
+            this.lstFile.Name = "lstFile";
+            this.lstFile.Size = new System.Drawing.Size(451, 277);
+            this.lstFile.TabIndex = 1;
+            this.lstFile.SelectedIndexChanged += new System.EventHandler(this.lstFile_SelectedIndexChanged);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(320, 43);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 2;
+            this.searchBtn.Text = "SEARCH";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // strpthexp
+            // 
+            this.strpthexp.Enabled = false;
+            this.strpthexp.Location = new System.Drawing.Point(401, 43);
+            this.strpthexp.Name = "strpthexp";
+            this.strpthexp.Size = new System.Drawing.Size(75, 23);
+            this.strpthexp.TabIndex = 3;
+            this.strpthexp.Text = "LOOK IN";
+            this.strpthexp.UseVisualStyleBackColor = true;
+            this.strpthexp.Click += new System.EventHandler(this.strpthexp_Click);
+            // 
+            // wholepccb
+            // 
+            this.wholepccb.AutoSize = true;
+            this.wholepccb.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.wholepccb.Location = new System.Drawing.Point(401, 20);
+            this.wholepccb.Name = "wholepccb";
+            this.wholepccb.Size = new System.Drawing.Size(77, 17);
+            this.wholepccb.TabIndex = 4;
+            this.wholepccb.Text = "All Memory";
+            this.wholepccb.UseVisualStyleBackColor = true;
+            this.wholepccb.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(505, 389);
+            this.Controls.Add(this.wholepccb);
+            this.Controls.Add(this.strpthexp);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.lstFile);
+            this.Controls.Add(this.txtQuery);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "File Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtQuery;
+        private System.Windows.Forms.ListBox lstFile;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button strpthexp;
+        private System.Windows.Forms.CheckBox wholepccb;
+    }
+}
+
---
+++ FileExplorer/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileExplorer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Explorer fileExplorer;
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fileExplorer = new Explorer();
+            wholepccb.Checked = true;
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            lstFile.Items.Clear();
+           
+            fileExplorer.search(txtQuery.Text);
+          
+        }
+
+        public class Explorer
+        {
+            static Form1 form = Application.OpenForms.OfType<Form1>().FirstOrDefault();
+
+            private string[] drives = null;
+            private string startpath = "wholecomputer";
+            private List<FileInfo> matchlist = null;
+            public bool print;
+
+            public string C = @"C:\";
+            public string D = @"D:\";
+
+
+            public Explorer()
+            {
+                this.drives = Directory.GetLogicalDrives();
+            }
+
+            public void renewmatchlist() { matchlist = new List<FileInfo>(); }
+            public void setstartpath(string path)
+            {
+                this.startpath = path;
+            }
+
+            public FileInfo getmatch(int index)
+            {
+                 return matchlist[index];
+            }
+
+            public FileInfo[] getmatchlist()
+            {
+                FileInfo[] templst = this.matchlist.ToArray();
+                return templst;
+            }
+
+            public FileInfo[] search(string path, string query)
+            {
+                setstartpath(path);
+                return search(query);
+            }
+
+            public FileInfo[] search(string query)
+            {
+                renewmatchlist();
+                pathclassifier(query);
+                return getmatchlist();
+            }
+            private void pathclassifier(string query)
+            {
+                if (startpath == "wholecomputer")
+                    foreach (string drive in drives)
+                    {
+                        recwander(drive, query);
+                    }
+                else
+                    recwander(startpath, query);
+            }
+
+            private bool recwander(string path, string query)
+            {
+                string[] directories;
+                string[] files;
+
+                try
+                {
+                    directories = Directory.GetDirectories(path);
+                    files = Directory.GetFiles(path);
+                }
+                catch { return false; }
+
+                foreach (string file in files)
+                {
+                    bool match = file.ToLower().Contains(query.ToLower());
+
+                    if (match)
+                    {
+                        FileInfo mf = new FileInfo(file);
+                        if (print) Console.WriteLine(mf.Name);
+                        form.lstFile.Items.Add(mf.Directory.Name+"\\"+mf.Name); 
+                        form.Refresh();
+                        this.matchlist.Add(mf);
+                    }
+ 
+                }
+
+                foreach (var directory in directories)
+                {
+                    recwander(directory, query);
+                }
+                return true;
+            }
+
+
+        }
+
+
+        private void lstFile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var index = lstFile.SelectedIndex;
+            Process.Start(fileExplorer.getmatch(index).Directory.FullName);
+        }
+
+        private void strpthexp_Click(object sender, EventArgs e)
+        {
+            var nfl = new FolderBrowserDialog();
+            var dr = nfl.ShowDialog();
+            fileExplorer.setstartpath(nfl.SelectedPath);
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (wholepccb.Checked)
+            {
+                fileExplorer.setstartpath("wholecomputer");
+                strpthexp.Enabled = false;
+            }
+            else
+            {
+                strpthexp.Enabled = true;
+            }
+        }
+    }
+   
+}
